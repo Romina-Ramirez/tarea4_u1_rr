57571db18d54a0281fb15864eac91ac52b448cc7
Romina-Ramirez <romina.maite.rt@gmail.com>
2022-05-30 13:05:26 -0500

    Tarea 4 - Spring Framework

---
+++ src/main/java/com/uce/edu/demo/matriculas/Estudiante.java
@@ -0,0 +1,36 @@
+package com.uce.edu.demo.matriculas;
+
+import java.time.LocalDateTime;
+
+public class Estudiante {
+	
+	private String nombre;
+	private String apellido;
+	private LocalDateTime fechaNacimiento;
+	
+	//Set y Get
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public LocalDateTime getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	
+	public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+}
---
+++ src/main/java/com/uce/edu/demo/matriculas/Matricula.java
@@ -0,0 +1,8 @@
+package com.uce.edu.demo.matriculas;
+
+public class Matricula {
+	
+	private Estudiante estudiante;
+	private Materia materia;
+
+}
